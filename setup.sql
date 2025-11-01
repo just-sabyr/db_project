5bebbaa317ee4e3cb45a3fec146633ba8f0ec9a7
just-sabyr <bazarymbetovsabyr@gmail.com>
2025-11-01 20:08:29 +0300

    modified:   setup.sql

--- setup.sql
+++ setup.sql
@@ -3,10 +3,13 @@ CREATE DATABASE IF NOT EXISTS db_project;
 USE db_project;
 
 
--- Extra table
+-- Extra tables
 CREATE TABLE IF NOT EXISTS genres (
     genre_id INT NOT NULL PRIMARY KEY,
     parent_genre VARCHAR(100),
     genre_name VARCHAR(100),
     genre_description TEXT
 );
+
+
+-- Main tables
